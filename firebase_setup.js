b9dab9d96ab64fe42991af0cd138eada0611f806
RHG <mailboxbrian.d@gmail.com>
2019-11-14 14:15:45 -0800

    added firebase_setup.js

--- firebase_setup.js
+++ firebase_setup.js
@@ -1,13 +1,21 @@
-        var firebaseConfig = {
-            apiKey: "AIzaSyB28Ev5egbzbVAD8insx3Y92CLf0UdRvG4",
-            authDomain: "comp1930-group10.firebaseapp.com",
-            databaseURL: "https://comp1930-group10.firebaseio.com",
-            projectId: "comp1930-group10",
-            storageBucket: "comp1930-group10.appspot.com",
-            messagingSenderId: "20075829482",
-            appId: "1:20075829482:web:88590a324dc4aa1f46df93",
-            measurementId: "G-6X7FP4G9T1"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB28Ev5egbzbVAD8insx3Y92CLf0UdRvG4",
+    authDomain: "comp1930-group10.firebaseapp.com",
+    databaseURL: "https://comp1930-group10.firebaseio.com",
+    projectId: "comp1930-group10",
+    storageBucket: "comp1930-group10.appspot.com",
+    messagingSenderId: "20075829482",
+    appId: "1:20075829482:web:88590a324dc4aa1f46df93",
+    measurementId: "G-6X7FP4G9T1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
         var db = firebase.firestore();
+
+        
+
+        
