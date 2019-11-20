9259f56f9b9957e65d1d845aa7126f3d95f080e0
Cragzu <cragzu@gmail.com>
2019-11-20 10:57:01 -0800

    Reworked displaying of components using JS

--- scripts/displayHeader.js
+++ scripts/displayHeader.js
@@ -1,4 +1,20 @@
 // Display header
-let link = document.querySelector('link[id=importHeader]');
-let content = link.import.querySelector('#mainHeader');
-document.body.appendChild(content.cloneNode(true));
+document.write(`
+    <div class="container-fluid" id="mainHeader">
+        <div class="row">
+            <div class="col-sm-4 headerColumn">
+                <a href="login.html"><button type="button" class="btn btn-primary">Login/Signup</button></a>
+            </div>
+    
+            <div class="col-sm-4 headerColumn">
+                <h1>StudySource</h1>
+            </div>
+    
+            <div class="col-sm-4 headerColumn">
+                <button type="button" class="btn btn-success">
+                    <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
+                </button>
+            </div>
+        </div>
+    </div>
+`);
