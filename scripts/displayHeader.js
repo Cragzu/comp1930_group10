c7116767b6d93a35aec23170584778062a55313d
Chloe (Desktop) <cragzu@gmail.com>
2019-11-20 16:40:25 -0800

    Reworked header and index.html

--- scripts/displayHeader.js
+++ scripts/displayHeader.js
@@ -2,6 +2,7 @@
 document.write(`
     <div class="container-fluid" id="mainHeader">
         <div class="row">
+        
             <div class="col-sm-4 headerColumn">
                 <a href="login.html"><button type="button" class="btn btn-primary">Login/Signup</button></a>
             </div>
@@ -12,10 +13,23 @@ document.write(`
             </div>
     
             <div class="col-sm-4 headerColumn">
-                <button type="button" class="btn btn-success">
-                    <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
-                </button>
+                <div class="collapse" id="navbarToggleExternalContent">
+                    <div class="p-4">
+                        <h5 class="text-white h4">Collapsed content</h5>
+                        <span class="text-muted">Toggleable via the navbar brand.</span>
+                    </div>
+                </div>
+                <nav class="navbar">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarToggleExternalContent"
+                            aria-controls="navbarToggleExternalContent" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span class="glyphicon glyphicon-menu-hamburger btn btn-primary" id="hamburgerMenu"
+                              aria-hidden="true"></span>
+                    </button>
+                </nav>
             </div>
+            
         </div>
     </div>
 `);
